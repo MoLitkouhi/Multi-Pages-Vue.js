e65c952a27f83a4c362ed86cd2bb89b6e9dd9380
Mohammad Litkouhi <mlit191@UoA.auckland.ac.nz>
2020-11-19 09:20:41 +1300

    Registered vue-router.

--- src/main.js
+++ src/main.js
@@ -1,7 +1,18 @@
 import { createApp } from 'vue';
+import { createRouter, createHistory } from 'vue-router'
 
 import App from './App.vue';
+import TeamsList from "@/components/teams/TeamsList";
+import UsersList from "@/components/users/UsersList";
 
-const app = createApp(App)
+const app = createApp(App);
+const router = createRouter({
+    history: createHistory(),
+    routes: [
+        {path: '/team', component: TeamsList},
+        {path: '/users', component: UsersList}
+    ]
+})
 
+app.use(router);
 app.mount('#app');
